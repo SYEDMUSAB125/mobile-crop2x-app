330e83c157134206498e4c667fb947e0048c39d9
Syed Musab <syedmasab125@gmail.com>
2025-01-08 10:28:32 +0500

    update saved data

--- hooks/useSaveData.js
+++ hooks/useSaveData.js
@@ -11,9 +11,16 @@ const useSaveData = () => {
     setError(null);
 
  let userName = email.split('@')[0];
-let version = 1.0;
+let version = "1.0";
     const { deviceId, temperature, conductivity, pH, moisture, nitrogen, phosphorus, potassium  } = data;
-
+    let time = new Date().getTime(); // Current timestamp
+    let date = new Date(time); // Convert timestamp to a Date object
+    
+    let hours = date.getHours().toString().padStart(2, '0'); // Hours (0-23) with leading zero
+    let minutes = date.getMinutes().toString().padStart(2, '0'); // Minutes (0-59) with leading zero
+    let seconds = date.getSeconds().toString().padStart(2, '0'); // Seconds (0-59) with leading zero
+    
+    let formattedTime = `${hours}:${minutes}:${seconds}`; // Format as "hh:mm:ss"
     if (!deviceId) {
       setError('Device ID is required to save data.');
       setLoading(false);
@@ -25,7 +32,7 @@ let version = 1.0;
   
       console.log("check",db)
       // Create a reference to the device path
-      const dataRef = ref(db, `devices/${userName}/${deviceId}`); // Example path: devices/M19-2407300001
+      const dataRef = ref(db, `devices/${userName}/${deviceId}/${formattedTime}`); // Example path: devices/M19-2407300001
 
       // Save data to Realtime Database
       await set(dataRef, {
